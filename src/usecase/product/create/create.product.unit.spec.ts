a99e8df261fcc2d437fefd24d8b440dd6f409deb
Willians <williansvf@hotmail.com>
2024-04-07 09:11:28 -0300

    first commit

---
+++ src/usecase/product/create/create.product.unit.spec.ts
@@ -0,0 +1,59 @@
+import ProductRepositoryInterface from "../../../domain/product/repository/product-repository.interface";
+import CreateProductUseCase from "./create.product.usecase";
+
+const MockRepository = () => {
+    return {
+        find: jest.fn(),
+        findAll: jest.fn(),
+        create: jest.fn(),
+        update: jest.fn(),
+    };
+};
+
+describe("Unit test create customer use case", () => {
+
+    it('Should create a product', async () => {
+        const productRepository = MockRepository();
+        const useCase = new CreateProductUseCase(productRepository);
+        const output = await useCase.execute({
+            name: 'Test',
+            type:'a',
+            price: 10
+        });
+
+        expect(output)
+        .toEqual({
+            id: expect.any(String),
+            name: 'Test',
+            price: 10
+        });
+
+
+    })
+
+    it('Should throw when name is missing', async () => {
+        const productRepository = MockRepository();
+        const useCase = new CreateProductUseCase(productRepository);
+
+        expect(useCase.execute({
+            name: '',
+            type:'a',
+            price: 10
+        })).rejects
+        .toThrow("Name is required");
+
+    })
+    it('Should throw when price is below zero', async () => {
+        const productRepository = MockRepository();
+        const useCase = new CreateProductUseCase(productRepository);
+
+        expect(useCase.execute({
+            name: 'Test',
+            type:'a',
+            price: -10
+        })).rejects
+        .toThrow("Price must be greater than zero");
+
+    })
+
+})
---
+++ src/usecase/product/create/create.product.usecase.ts
@@ -0,0 +1,27 @@
+import { InputCreateProductDto, OutputCreateProductDto } from "./create.product.dto";
+import ProductRepositoryInterface from "../../../domain/product/repository/product-repository.interface"
+import ProductFactory from "../../../domain/product/factory/product.factory";
+import Product from "../../../domain/product/entity/product";
+
+export default class CreateProductUseCase{
+
+    private repository:ProductRepositoryInterface;
+
+
+    constructor(repository: ProductRepositoryInterface){
+        this.repository = repository;
+    }
+
+    async execute(input: InputCreateProductDto): Promise<OutputCreateProductDto>{
+        const product = ProductFactory.create(input.type,
+            input.name, input.price)
+
+        await this.repository.create(product as Product);    
+        return {
+            id: product.id,
+            name: product.name,
+            price: product.price
+        };
+    }
+
+}
