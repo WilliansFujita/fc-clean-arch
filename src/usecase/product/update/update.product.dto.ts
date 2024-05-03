75d75c382c826b0caad87f6bfbf7817cf0f99043
Willians <williansvf@hotmail.com>
2024-05-03 16:26:27 -0300

    feat: update product usecase

---
+++ src/usecase/product/update/update.product.dto.ts
@@ -0,0 +1,12 @@
+
+export interface InputUpdateProductDto {
+    id: String,
+    name: string,
+    price: number
+}
+
+export interface OutputUpdateProductDto {
+    id: string,
+    name: string,
+    price: number
+}
---
+++ src/usecase/product/update/update.product.unit.spec.ts
@@ -0,0 +1,31 @@
+import Product from "../../../domain/product/entity/product"
+import UpdateProductUseCase from "./update.product.use.case"
+
+const product = new Product("123", "teste", 10)
+
+const input = {
+    id: product.id,
+    name: "Teste2",
+    price: 20
+}
+
+const MockRepository = () => {
+
+    return {
+        find: jest.fn().mockReturnValue(product),
+        findAll: jest.fn(),
+        create: jest.fn(),
+        update: jest.fn()
+    }
+}
+
+describe('Update Product Use Case Unit Test', () => {
+
+    test('Should update a product', async () => {
+        const usecase = new UpdateProductUseCase(MockRepository())
+
+        const output = await usecase.execute(input)
+
+        expect(output).toBe(input)
+    })
+})
