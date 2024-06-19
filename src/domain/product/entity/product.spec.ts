5dec48e31c3b8c5639cd69b15a7d77cc4957144d
Willians <williansvf@hotmail.com>
2024-06-19 13:19:23 -0300

    feat: products error notification

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -2,21 +2,34 @@ import Product from "./product";
 
 describe("Product unit tests", () => {
   it("should throw error when id is empty", () => {
-    expect(() => {
-      const product = new Product("", "Product 1", 100);
-    }).toThrowError("Id is required");
+    const product = new Product("", "Product 1", 100);
+
+    expect(product.notification.hasErrors()).toBe(true)
+    expect(product.notification.getErrors()[0].message).toBe("Id is required")
+    
   });
 
   it("should throw error when name is empty", () => {
-    expect(() => {
-      const product = new Product("123", "", 100);
-    }).toThrowError("Name is required");
+    const product = new Product("123", "", 100);
+
+    expect(product.notification.hasErrors()).toBe(true)
+    expect(product.notification.getErrors()[0].message).toBe("Name is required")
+  
   });
 
   it("should throw error when price is less than zero", () => {
-    expect(() => {
-      const product = new Product("123", "Name", -1);
-    }).toThrowError("Price must be greater than zero");
+    const product = new Product("123", "Name", -1);
+    expect(product.notification.hasErrors()).toBe(true)
+    expect(product.notification.getErrors()[0].message).toBe("Price must be greater than zero")
+  });
+
+  it("should throw errors when name is empty and id is empty", () => {
+    const product = new Product("", "", 100);
+
+    expect(product.notification.hasErrors()).toBe(true)
+    expect(product.notification.getErrors()[0].message).toBe("Id is required")
+    expect(product.notification.getErrors()[1].message).toBe("Name is required")
+  
   });
 
   it("should change name", () => {
